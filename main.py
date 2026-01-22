93e0531e5d29c59a60c46719279f3cd7faad0338
Andrew Andrey <graal101@mail.ru>
2026-01-22 15:09:51 +0700

    work wit login

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', title='Домашняя страница')
+    
+@app.route('/login')
+def login():
+    return render_template('login.html', title='Авторизация')
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
