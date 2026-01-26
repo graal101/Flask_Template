9f5e0b9a85867b0ca3cac45dee7c26aa3f7f60ab
Andrew Andrey <graal101@mail.ru>
2026-01-26 13:29:36 +0700

    added 404 page

--- main.py
+++ main.py
@@ -4,15 +4,22 @@ from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
+
 @app.route('/')
 @app.route('/index')
 def home():
     return render_template('index.html', title='Домашняя страница')
-    
+
+
 @app.route('/login')
 def login():
     return render_template('login.html', title='Авторизация')
-    
+
+
+@app.errorhandler(404)
+def pageNotFound(error):
+    return render_template('404.html', title='404 - Страница не найдена')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
